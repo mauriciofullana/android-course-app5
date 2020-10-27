a078433019eb3d1214967ad92c473c51c8d2300f
Mauricio Fullana <mfullana4@gmail.com>
2020-10-26 22:52:40 -0300

    BottomTabNavigation.

--- app/src/main/java/com/example/menus/SecondActivity.java
+++ app/src/main/java/com/example/menus/SecondActivity.java
@@ -1,10 +1,12 @@
 package com.example.menus;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -12,7 +14,9 @@ import androidx.navigation.ui.NavigationUI;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 public class SecondActivity extends AppCompatActivity {
@@ -21,6 +25,7 @@ public class SecondActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private NavController navController;
     private AppBarConfiguration appBarConfiguration;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +35,23 @@ public class SecondActivity extends AppCompatActivity {
         navigationView = findViewById(R.id.main_navigation_view);
         navController = Navigation.findNavController(this, R.id.main_nav_host);
         drawerLayout = findViewById(R.id.main_drawer);
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
 
         appBarConfiguration = new AppBarConfiguration.Builder(R.id.homeFragment, R.id.aboutUsFragment, R.id.profileFragment, R.id.cartFragment).setDrawerLayout(drawerLayout).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+                if (destination.getId() == R.id.cartFragment) {
+                    bottomNavigationView.setVisibility(View.GONE);
+                } else {
+                    bottomNavigationView.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 
     @Override
