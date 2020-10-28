d6d016d063dbd6ba04dc4a537f9f0ad4456ee339
Mauricio Fullana <mfullana4@gmail.com>
2020-10-28 15:45:11 -0300

    Alert Dialog

--- app/src/main/java/com/example/menus/BottomBarActivity.java
+++ app/src/main/java/com/example/menus/BottomBarActivity.java
@@ -1,11 +1,14 @@
 package com.example.menus;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
@@ -15,10 +18,70 @@ public class BottomBarActivity extends AppCompatActivity {
 
     private BottomAppBar bottomAppBar;
     private FloatingActionButton fab;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_bar);
+
+        Button btnShowAlertDialog = findViewById(R.id.btn_showAlertDialog);
+        Button btnShowAlertOption = findViewById(R.id.btn_showAlertOption);
+
+        btnShowAlertDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(BottomBarActivity.this);
+                alertDialog.setTitle("Delete this file");
+                alertDialog.setCancelable(false);
+
+                alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(BottomBarActivity.this, "Yes Clicked", Toast.LENGTH_SHORT).show();
+                        dialog.cancel();
+                    }
+                });
+
+                alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(BottomBarActivity.this, "No Clicked", Toast.LENGTH_SHORT).show();
+                        dialog.cancel();
+                    }
+                });
+
+                alertDialog.show();
+            }
+        });
+
+        btnShowAlertOption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(BottomBarActivity.this);
+                alertDialog.setTitle("Select option");
+                alertDialog.setCancelable(false);
+
+                CharSequence options[] = new CharSequence[]{"Option 1", "Option 2", "Option 3"};
+                alertDialog.setItems(options, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0:
+                                Toast.makeText(BottomBarActivity.this, "Option 1 clicked", Toast.LENGTH_SHORT).show();
+                                break;
+                            case 1:
+                                Toast.makeText(BottomBarActivity.this, "Option 2 clicked", Toast.LENGTH_SHORT).show();
+                                break;
+                            case 2:
+                                Toast.makeText(BottomBarActivity.this, "Option 3 clicked", Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+                    }
+                });
+
+                alertDialog.show();
+            }
+        });
 
         fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -27,7 +90,7 @@ public class BottomBarActivity extends AppCompatActivity {
                 Toast.makeText(BottomBarActivity.this, "Fab clicked", Toast.LENGTH_SHORT).show();
             }
         });
-        
+
         bottomAppBar = findViewById(R.id.bottomAppBar);
         bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
